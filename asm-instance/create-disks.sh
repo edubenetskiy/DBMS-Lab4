14bd9ce8610aceb52a85b87a0780d5e1dc61246c
Egor Dubenetskiy <egor@banka.space>
2019-12-26 14:42:57 +0300

    Create disks

---
+++ asm-instance/create-disks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+
+DISK_ROOT=/u01
+
+createDisk() {
+    local name=$1
+    local numDisks=$2
+    local size=$3
+    mkdir -p $DISK_ROOT/$name
+    for ((diskIndex = 0; diskIndex < $numDisks; diskIndex++))
+    do
+        local diskPath=$name/$name$diskIndex
+        /usr/sbin/mkfile -n $size $diskPath
+    done
+}
+
+createDisk cutegoat 3 500m
+createDisk carelesscat 7 500m
